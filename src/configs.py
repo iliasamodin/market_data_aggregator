50a8d242cb88ed354db2e98a8602341475a0a77a
Ilia Samodin <broobooot@gmail.com>
2026-03-15 22:16:43 +0600

    feat(app): Initial commit

---
+++ src/configs.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    # API
+    HOST: str = "0.0.0.0"
+    PORT: int = 1500
+    API_PREFIX: str = "/api"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
